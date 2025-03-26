b5266a39a664448834e546ac37f0e1f6096c5193
Murunwa <134610001+MurunwaMaphiri1@users.noreply.github.com>
2025-03-26 12:23:40 +0200

    Fixed routing on Movies page

--- src/Movies.jsx
+++ src/Movies.jsx
@@ -7,6 +7,7 @@ export default function Movies() {
     const [error, setError] = useState("");
     let [pageNumber, setPageNumber] = useState(1);
     const [pages, setPages] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         const getMovies = async() => {
@@ -30,6 +31,10 @@ export default function Movies() {
         }
         getMovies();
     }, [pageNumber]);
+
+    const handleAnimeClick = (id) => {
+        navigate(`/anime/${id}`)
+    }
 
     const handlePageChange = (totalItems) => {
         const totalPages = Math.ceil(totalItems/24);
@@ -60,6 +65,7 @@ export default function Movies() {
                     <div
                         className="anime-card"
                         key={anime.mal_id}
+                        onClick={() => handleAnimeClick(anime.mal_id)}
                         style={{ cursor: 'pointer' }}
                     >
                         <img
